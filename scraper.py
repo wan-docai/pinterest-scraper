4c2bd77debd711cd67b6b0851cf932e0147a96a6
Ankit Shekhawat <contact@ankit.ws>
2017-05-10 13:42:30 +0530

    + added a caching functionality using cPickle, runme() now stores all the urls in .p files  + added download function which opens the pickle file and download the files  + use MD5 checkusm to ensure not to redownload or duplicate. files are saved with the MD5 checksum of the original URL  + added three optional parameter to Pinterest_helper object constructor 	= label (default:cache): Cache defines the pickle cache file name and download sub directory. 	= directory (default:data): Directory you want to download the files to. Defaults to ./data/ 	= browser (default:True): option to disable instanciating browser(useful if only downloading the data from a cache file. Will break if using for runme function) ~ Pinterest now uses /474x/ files insted of /236x/ . So changed it to 474x in the url search ~ Changed to chromedriver
    TODO:
    	remove dependencies on pandas as pickle and csv can be used on thier own.

--- scraper.py
+++ scraper.py
@@ -4,6 +4,13 @@ from selenium.webdriver.common.keys import Keys
 import time,random,socket,unicodedata
 import string, copy
 import pandas as pd
+import cPickle as pickle
+import hashlib
+import time
+import requests
+from urlparse import urlparse
+import os.path
+import os
 
 def randdelay(a,b):
     time.sleep(random.uniform(a,b))
@@ -13,15 +20,19 @@ def u_to_s(uni):
 
 class Pinterest_Helper(object):
     
-    def __init__(self, login, pw):
-        self.browser = webdriver.Firefox()
-        self.browser.get("https://www.pinterest.com")
-        emailElem = self.browser.find_element_by_name('id')
-        emailElem.send_keys(login)
-        passwordElem = self.browser.find_element_by_name('password')
-        passwordElem.send_keys(pw)
-        passwordElem.send_keys(Keys.RETURN)
-        randdelay(2,4)
+    def __init__(self, login, pw, label='cache', directory='data',browser=True):
+        self.label = label
+        self.directory = './'+ directory + '/'
+        if browser:
+            # self.browser = webdriver.Firefox()
+            self.browser = webdriver.Chrome('./chromedriver') # you need to download chrome driver
+            self.browser.get("https://www.pinterest.com")            
+            emailElem = self.browser.find_element_by_name('id')
+            emailElem.send_keys(login)
+            passwordElem = self.browser.find_element_by_name('password')
+            passwordElem.send_keys(pw)
+            passwordElem.send_keys(Keys.RETURN)
+            randdelay(2,4)
     
     def getURLs(self, urlcsv, threshold = 500):
         tmp = self.read(urlcsv)
@@ -35,6 +46,8 @@ class Pinterest_Helper(object):
     def write(self, myfile, mylist):
         tmp = pd.DataFrame(mylist)
         tmp.to_csv(myfile, index=False, header=False)
+
+
     
     def read(self,myfile):
         tmp = pd.read_csv(myfile,header=None).values.tolist()
@@ -43,11 +56,18 @@ class Pinterest_Helper(object):
             tmp2.append(tmp[i][0])
         return tmp2        
         
-    
+
+
     def runme(self,url, threshold = 500):
         final_results = []
         previmages = []
         tries = 0
+        try:
+            data = pickle.load( open( self.label + ".p", "rb" ) )
+        except Exception as e:
+            open(self.label + ".p", 'a').close()
+            data ={}
+
         try:
             self.browser.get(url)
             while threshold > 0:
@@ -62,9 +82,13 @@ class Pinterest_Helper(object):
                         return final_results
                     for i in images:
                         src = i.get_attribute("src")
+
                         if src:
-                            if string.find(src,"/236x/") != -1:
-                                src = string.replace(src,"/236x/","/736x/")
+                            if string.find(src,"/474x/") != -1:
+                                print src        
+                                src = string.replace(src,"/474x/","/736x/")
+                                data[hashlib.md5(u_to_s(src)).hexdigest()] = u_to_s(src)
+                                pickle.dump( data, open( self.label+".p", "wb" ) )                               
                                 results.append(u_to_s(src))
                     previmages = copy.copy(images)
                     final_results = list(set(final_results + results))
@@ -78,7 +102,35 @@ class Pinterest_Helper(object):
         except (socket.error, socket.timeout):
             pass
         return final_results
-        
+
+    def download(self):
+        """Download all the labels in file <label>.p"""
+        try:
+            data = pickle.load( open( self.label + ".p", "rb" ) )
+        except Exception as e:
+            raise e
+        else:
+            try:
+                os.stat(self.directory+ self.label)
+            except:
+                os.mkdir(self.directory+ self.label)   
+            for d in (data.keys()):
+                print data[d]
+                if not os.path.isfile(self.directory+ self.label + '/'+ d +'.jpg'):
+                    try:
+                        img_data = requests.get(data[d]).content
+                        with open(self.directory+ self.label+'/'+ d + '.jpg', 'wb') as handler:
+                            handler.write(img_data)
+                    except Exception as e:
+                        print(e)
+                        time.sleep(3)
+                    else:
+                        data.pop(d)
+                        pickle.dump( data, open( self.label + ".p", "wb" ) )
+                elif os.path.isfile(self.directory+ self.label + '/'+ d +'.jpg'):
+                    data.pop(d)
+                    pickle.dump( data, open( self.label + ".p", "wb" ) )                                            
+       
  
     def scrape_old(self, url):
         results = []
@@ -87,8 +139,8 @@ class Pinterest_Helper(object):
         for i in images:
             src = i.get_attribute("src")
             if src:
-                if string.find(src,"/236x/") != -1:
-                    src = string.replace(src,"/236x/","/736x/")
+                if string.find(src,"/474x/") != -1:
+                    src = string.replace(src,"/474x/","/736x/")
                     results.append(u_to_s(src))
         return results
             
