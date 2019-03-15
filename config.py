0ff96120215e7a09921e5d4372204e08ccf127b6
Wan Zhu <wan@Wans-MacBook-Pro.local>
2019-03-15 09:07:43 -0700

    update account

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Pinterest Settings
-PINTEREST_USERNAME = 'wan@doc.ai'
+PINTEREST_USERNAME = 'pinterest1234@mailinator.com'
 
-PINTEREST_PASSWORD = 'wan@doc.ai'
+PINTEREST_PASSWORD = 'pinterest1234'
