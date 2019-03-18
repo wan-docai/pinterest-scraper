9ce0b3cf747dc13ba3e409c8c2eb1a545932d3f4
Wan Zhu <wan@wans-mbp.lan>
2019-03-17 22:00:34 -0700

    update download_image.py to use for unhealthy image download

--- download_image.py
+++ download_image.py
@@ -3,10 +3,10 @@ import csv
 
 #change txt name to merge healthy/unhealthy big file
 ## double check save path before process
-with open ('Fruits_dish_pins_test.txt') as images:
+with open ('unhealthy.csv') as images:
     images = csv.reader(images)
     img_count = 1
     for image in images:
         urlretrieve(image[0],
-                'images/healthy/image_{0}.jpg'.format(img_count))
+                'images/unhealthy/pinterest_image_{0}.jpg'.format(img_count))
         img_count += 1
