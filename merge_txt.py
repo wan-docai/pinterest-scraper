5706897e40bca052077a3de15d64781203fe4d10
Wan Zhu <wan@Wans-MacBook-Pro.local>
2019-03-26 11:47:12 -0700

    update txt file name

--- merge_txt.py
+++ merge_txt.py
@@ -1,9 +1,9 @@
 import glob
 
-read_files = glob.glob("./image_url/healthy/*.txt")
+read_files = glob.glob("./image_url/not_food/*.txt")
 
 # modify image path and output file name when use
-with open("healthy.csv", "wb") as outfile:
+with open("not_food.csv", "wb") as outfile:
     for f in read_files:
         with open(f, "rb") as infile:
             outfile.write(infile.read())
